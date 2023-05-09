08ee0e3815bcc449d90c491581f12d4743752432
Supreme Ciento <35246400+CloudGang@users.noreply.github.com>
2023-05-09 13:15:54 -0400

    Update o1o.py

--- o1o.py
+++ o1o.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import os import sys
+import os 
+import sys
 github_repo = 'https://github.com/CloudGang/q-e-d-g4-g-6-7-a-123-g-5-75-f-323-f-4-c-dw-3-f-wd-2-fs-f-3-/tree/main'
 file_path = os.path.join(github_repo, 'o.txt')
 with open(file_path, 'r') as f: 
