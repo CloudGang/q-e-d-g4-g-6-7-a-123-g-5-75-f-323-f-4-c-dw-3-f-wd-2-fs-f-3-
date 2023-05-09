d9b268063b3fa9065d66646a8d4180a92ddc08ec
Supreme Ciento <35246400+CloudGang@users.noreply.github.com>
2023-05-09 11:54:19 -0400

    Update x1x.py

--- x1x.py
+++ x1x.py
@@ -1,7 +1,12 @@
+import streamlit as st
+
 def x1x():
-  B='o.txt';C=open(B,'r');D=C.readline();print(D)
+  B='o.txt';C=open(B,'r');D=C.readline();st.write("Save Your Key!");st.write(f":orange[Black-Key Code: ]:green[{D}]")
   with open(B,'r')as A:
     E=A.readlines()
     with open(B,'w')as A:
       for F in E[1:]:A.write(F)
-x1x()
+x = st.button('Get Black-Key: ')
+if x:
+  x(disabled=True)
+  x1x()
