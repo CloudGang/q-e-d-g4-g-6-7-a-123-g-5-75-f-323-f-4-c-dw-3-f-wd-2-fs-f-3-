ffe50639bfa7c994be8e5e931b43fed18f2b8884
Supreme Ciento <35246400+CloudGang@users.noreply.github.com>
2023-05-09 14:06:16 -0400

    Update x1x.py

--- x1x.py
+++ x1x.py
@@ -1,17 +1,21 @@
 import streamlit as st
 
 def x1x():
-  B='o.txt';C=open(B,'r');D=C.readline();st.write("Save Your Key!");st.write("If you see Brackets :red[[ ]], Your code is :red[ [:green[BETWEEN] ]] the brackets.")
-  with col2:
-    st.write(f":orange[Black-Key Code: ]:green[{D}]")
-  with col3:
-    em = st.text_input("Enter your Email to reieve Key")
-    html_string = f'<a href="mailto:{em}?subject=Black-Key Code{D}&body={D}">Click to send Email</a>'
-    st.markdown(html_string, unsafe_allow_html=True)
-  with open(B,'r')as A:
-    E=A.readlines()
-    with open(B,'w')as A:
-      for F in E[1:]:A.write(F)
+  B='o.txt';C=open(B,'r');D=C.readline()
+  if "!NO" in D:
+    st.write('No Key, Contact Site Admin')
+  else:
+    st.write("Save Your Key!");st.write("If you see Brackets :red[[ ]], Your code is :red[ [:green[BETWEEN] ]] the brackets.")
+    with col2:
+      st.write(f":orange[Black-Key Code: ]:green[{D}]")
+    with col3:
+      em = st.text_input("Enter your Email to reieve Key")
+      html_string = f'<a href="mailto:{em}?subject=Black-Key Code{D}&body={D}">Click to send Email</a>'
+      st.markdown(html_string, unsafe_allow_html=True)
+    with open(B,'r')as A:
+      E=A.readlines()
+      with open(B,'w')as A:
+        for F in E[1:]:A.write(F)
 
 cust = ["BBW6843311"]
 ID = st.text_input("Enter your Paypal Item ID#","ie: BB3871")
@@ -23,9 +27,6 @@ with col1:
     isclick = placeholder.button('Check ID')
     if isclick:
         placeholder.empty()
-        if "!NO" in B:
-          st.write('No Key, Contact Site Admin')
-        else:
           x1x()
 
 
